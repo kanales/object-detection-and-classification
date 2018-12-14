c269744f1a11eae2313710c529b622bc4c0f5232
kanales <ivancanalestgna@gmail.com>
2018-12-14 13:38:34 +0100

    Open show save

---
+++ src/config.h
@@ -0,0 +1,14 @@
+//
+//  config.h
+//  object-detection-and-classification
+//
+//  Created by Iván Canales Martín on 14/12/2018.
+//  Copyright © 2018 Iván Canales Martín. All rights reserved.
+//
+
+#ifndef config_h
+#define config_h
+
+#define $ROOT "/Users/kanales/Documents/Uni/TDCV/object-detection-and-classification/object-detection-and-classification/"
+
+#endif /* config_h */
--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,39 @@
 //  Created by Iván Canales Martín on 14/12/2018.
 //  Copyright © 2018 Iván Canales Martín. All rights reserved.
 //
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
 
 #include <iostream>
+#include <string>
+
+#include "config.h"
+
+#define XD lol
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    cv::String imageName( $ROOT "data/task1/obj1000.jpg" );
+    
+    if( argc > 1)
+    {
+        imageName = argv[1];
+    }
+    
+    cv::Mat image;
+    image = cv::imread(imageName, cv::IMREAD_COLOR);
+    
+    if( image.empty() )                      // Check for invalid input
+    {
+        std::cout <<  "Could not open or find the image" << std::endl ;
+        return -1;
+    }
+
+    cv::namedWindow( "Display window", cv::WINDOW_AUTOSIZE ); // Create a window for display.
+
+    cv::imshow("Display window", image);
+    
+    cv::imwrite($ROOT "output/test.jpg", image);
+    cv::waitKey(0);
     return 0;
 }
