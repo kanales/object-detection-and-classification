d761543875a191d14ddb7b0441d43f151648426a
kanales <ivancanalestgna@gmail.com>
2018-12-14 12:19:22 +0100

    Initial Commit

---
+++ Object detection and classification/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Object detection and classification
+//
+//  Created by Iván Canales Martín on 14/12/2018.
+//  Copyright © 2018 Iván Canales Martín. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
