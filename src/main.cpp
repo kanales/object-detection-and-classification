55446882e399d7f56efaad266b835d1c426a119c
mellifluo <mellifluo@github.com>
2019-01-04 12:25:59 +0100

    augmenting data and starting task3

--- src/main.cpp
+++ src/main.cpp
@@ -5,20 +5,13 @@
 //  Created by Iván Canales Martín on 14/12/2018.
 //  Copyright © 2018 Iván Canales Martín. All rights reserved.
 //
-#include <opencv2/opencv.hpp>
 
-#include <iostream>
-#include <vector>
-#include <string>
-
-#include "config.h"
-#include "utils.h"
 #include "task1.h"
 #include "task2.h"
-#include "RandomForest.hpp"
-#include "ObjectDetector.h"
+#include "task3.h"
 
 int main(int argc, const char * argv[]) {
     part1(argc,argv);
     part2(argc, argv);
+    part3(argc, argv);
 }
--- src/utils.h
+++ src/utils.h
@@ -1,6 +1,7 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <opencv2/opencv.hpp>
 #include <dirent.h>
 #include <iostream>
 #include <sys/types.h>
